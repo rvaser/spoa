699350c8fa79a44ae2cbd9df5033966d92fdf4b4
Botond Sipos <Botond.Sipos@nanoporetech.com>
2020-02-12 15:02:55 +0000

    Added file: serialize.h

---
+++ include/spoa/serialize.h
@@ -0,0 +1,13 @@
+#ifndef SERIALIZE_H_INCLUDED
+#define SERIALIZE_H_INCLUDED
+
+#define CEREAL_THREAD_SAFE 1
+#include <cereal/access.hpp>
+#include <cereal/archives/binary.hpp>
+#include <cereal/cereal.hpp>
+#include <cereal/types/map.hpp>
+#include <cereal/types/memory.hpp>
+#include <cereal/types/unordered_map.hpp>
+#include <cereal/types/vector.hpp>
+
+#endif
