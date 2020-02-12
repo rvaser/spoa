e0bb60c119a0d42a01cac662b8e35bb42e83f3ef
Botond Sipos <Botond.Sipos@nanoporetech.com>
2020-02-12 14:48:04 +0000

    Added serialization based on the cereal library.

--- include/spoa/graph.hpp
+++ include/spoa/graph.hpp
@@ -9,9 +9,10 @@
 #include <cstdint>
 #include <memory>
 #include <string>
-#include <vector>
-#include <utility>
 #include <unordered_set>
+#include <utility>
+#include <vector>
+#include "spoa/serialize.h"
 
 namespace spoa {
 
@@ -27,59 +28,54 @@ class Graph {
 public:
     ~Graph();
 
-    const std::vector<std::unique_ptr<Node>>& nodes() const {
-        return nodes_;
+    const std::vector<std::unique_ptr<Node>>& nodes() const { return nodes_; }
+
+    const std::vector<std::uint32_t>& rank_to_node_id() const
+    {
+	return rank_to_node_id_;
     }
 
-    const std::vector<std::uint32_t>& rank_to_node_id() const {
-        return rank_to_node_id_;
-    }
+    std::uint32_t num_codes() const { return num_codes_; };
 
-    std::uint32_t num_codes() const {
-        return num_codes_;
-    };
+    std::uint8_t coder(std::uint8_t c) const { return coder_[c]; }
 
-    std::uint8_t coder(std::uint8_t c) const {
-        return coder_[c];
-    }
-
-    std::uint8_t decoder(std::uint8_t code) const {
-        return decoder_[code];
-    }
+    std::uint8_t decoder(std::uint8_t code) const { return decoder_[code]; }
 
     void add_alignment(const Alignment& alignment, const std::string& sequence,
-        std::uint32_t weight = 1);
+		       std::uint32_t weight = 1);
 
     void add_alignment(const Alignment& alignment, const char* sequence,
-        std::uint32_t sequence_size, std::uint32_t weight = 1);
+		       std::uint32_t sequence_size, std::uint32_t weight = 1);
 
     void add_alignment(const Alignment& alignment, const std::string& sequence,
-        const std::string& quality);
+		       const std::string& quality);
 
     void add_alignment(const Alignment& alignment, const char* sequence,
-        std::uint32_t sequence_size, const char* quality,
-        std::uint32_t quality_size);
+		       std::uint32_t sequence_size, const char* quality,
+		       std::uint32_t quality_size);
 
     void add_alignment(const Alignment& alignment, const std::string& sequence,
-        const std::vector<std::uint32_t>& weights);
+		       const std::vector<std::uint32_t>& weights);
 
     void add_alignment(const Alignment& alignment, const char* sequence,
-        std::uint32_t sequence_size, const std::vector<std::uint32_t>& weights);
+		       std::uint32_t sequence_size,
+		       const std::vector<std::uint32_t>& weights);
 
     void generate_multiple_sequence_alignment(std::vector<std::string>& dst,
-        bool include_consensus = false);
+					      bool include_consensus = false);
 
     std::string generate_consensus();
     // returns  base coverages or complete summary matrix if verbose equals true
     std::string generate_consensus(std::vector<std::uint32_t>& dst,
-        bool verbose = false);
+				   bool verbose = false);
 
-    std::unique_ptr<Graph> subgraph(std::uint32_t begin_node_id,
-        std::uint32_t end_node_id,
-        std::vector<std::int32_t>& subgraph_to_graph_mapping) const;
+    std::unique_ptr<Graph> subgraph(
+	std::uint32_t begin_node_id, std::uint32_t end_node_id,
+	std::vector<std::int32_t>& subgraph_to_graph_mapping) const;
 
-    void update_alignment(Alignment& alignment,
-        const std::vector<std::int32_t>& subgraph_to_graph_mapping) const;
+    void update_alignment(
+	Alignment& alignment,
+	const std::vector<std::int32_t>& subgraph_to_graph_mapping) const;
 
     void print_dot(const std::string& path) const;
 
@@ -87,24 +83,34 @@ public:
 
     friend std::unique_ptr<Graph> createGraph();
 
+    template <class Archive>
+    void serialize(Archive& archive)
+    {
+	archive(num_sequences_, num_codes_, coder_, decoder_, nodes_,
+		rank_to_node_id_, sequences_begin_nodes_ids_, consensus_);
+    }
+
 private:
     Graph();
     Graph(const Graph&) = delete;
     const Graph& operator=(const Graph&) = delete;
 
-    static std::unique_ptr<Node> createNode(std::uint32_t id, std::uint32_t code);
+    static std::unique_ptr<Node> createNode(std::uint32_t id,
+					    std::uint32_t code);
 
     static std::unique_ptr<Edge> createEdge(std::uint32_t begin_node_id,
-        std::uint32_t end_node_id, std::uint32_t label, std::uint32_t weight);
+					    std::uint32_t end_node_id,
+					    std::uint32_t label,
+					    std::uint32_t weight);
 
     std::uint32_t add_node(std::uint32_t code);
 
     void add_edge(std::uint32_t begin_node_id, std::uint32_t end_node_id,
-        std::uint32_t weight);
+		  std::uint32_t weight);
 
     std::int32_t add_sequence(const char* sequence,
-        const std::vector<std::uint32_t>& weights, std::uint32_t begin,
-        std::uint32_t end);
+			      const std::vector<std::uint32_t>& weights,
+			      std::uint32_t begin, std::uint32_t end);
 
     void topological_sort();
 
@@ -113,14 +119,15 @@ private:
     void traverse_heaviest_bundle();
 
     std::uint32_t branch_completion(std::vector<std::int64_t>& scores,
-        std::vector<std::int32_t>& predecessors,
-        std::uint32_t rank);
+				    std::vector<std::int32_t>& predecessors,
+				    std::uint32_t rank);
 
     void extract_subgraph_nodes(std::vector<bool>& dst,
-        std::uint32_t current_node_id, std::uint32_t end_node_id) const;
+				std::uint32_t current_node_id,
+				std::uint32_t end_node_id) const;
 
     std::uint32_t initialize_multiple_sequence_alignment(
-        std::vector<std::uint32_t>& dst) const;
+	std::vector<std::uint32_t>& dst) const;
 
     std::uint32_t num_sequences_;
     std::uint32_t num_codes_;
@@ -130,30 +137,29 @@ private:
     std::vector<std::uint32_t> rank_to_node_id_;
     std::vector<std::uint32_t> sequences_begin_nodes_ids_;
     std::vector<std::uint32_t> consensus_;
-};
+};  // namespace spoa
 
 class Node {
 public:
     ~Node();
 
-    std::uint32_t id() const {
-        return id_;
+    std::uint32_t id() const { return id_; }
+
+    std::uint32_t code() const { return code_; }
+
+    const std::vector<std::shared_ptr<Edge>>& in_edges() const
+    {
+	return in_edges_;
     }
 
-    std::uint32_t code() const {
-        return code_;
+    const std::vector<std::shared_ptr<Edge>>& out_edges() const
+    {
+	return out_edges_;
     }
 
-    const std::vector<std::shared_ptr<Edge>>& in_edges() const {
-        return in_edges_;
-    }
-
-    const std::vector<std::shared_ptr<Edge>>& out_edges() const {
-        return out_edges_;
-    }
-
-    const std::vector<std::uint32_t>& aligned_nodes_ids() const {
-        return aligned_nodes_ids_;
+    const std::vector<std::uint32_t>& aligned_nodes_ids() const
+    {
+	return aligned_nodes_ids_;
     }
 
     bool successor(std::uint32_t& dst, std::uint32_t label) const;
@@ -161,6 +167,12 @@ public:
     std::uint32_t coverage() const;
 
     friend Graph;
+
+    template <class Archive>
+    void serialize(Archive& archive)
+    {
+	archive(id_, code_, in_edges_, out_edges_, aligned_nodes_ids_);
+    }
 
 private:
     Node(std::uint32_t id, std::uint32_t code);
@@ -178,20 +190,22 @@ class Edge {
 public:
     ~Edge();
 
-    std::uint32_t begin_node_id() const {
-        return begin_node_id_;
-    }
+    std::uint32_t begin_node_id() const { return begin_node_id_; }
 
-    std::uint32_t end_node_id() const {
-        return end_node_id_;
-    }
+    std::uint32_t end_node_id() const { return end_node_id_; }
 
     friend Graph;
     friend Node;
 
+    template <class Archive>
+    void serialize(Archive& archive)
+    {
+	archive(begin_node_id_, end_node_id_, sequence_labels_, total_weight_);
+    }
+
 private:
     Edge(std::uint32_t begin_node_id, std::uint32_t end_node_id,
-        std::uint32_t label, std::uint32_t weight);
+	 std::uint32_t label, std::uint32_t weight);
     Edge(const Edge&) = delete;
     const Edge& operator=(const Edge&) = delete;
 
@@ -203,4 +217,4 @@ private:
     std::int64_t total_weight_;
 };
 
-}
+}  // namespace spoa
